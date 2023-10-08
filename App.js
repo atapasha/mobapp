9db4602a383dc4bf427ab89172129365a71c5a5e
atapasha <atapashazade@gmail.com>
2023-10-08 23:11:22 +0330

    developed by reactnative

---
+++ App.js
@@ -0,0 +1,33 @@
+import { View,Text,Platform  } from "react-native";
+import Home from './home/index.android'
+
+function App(){
+
+  return (
+
+
+    <Home />
+
+  );
+
+}
+
+
+const textstyle={
+  color:'white'
+}
+
+ 
+
+const styles= ({
+
+  flex:1,
+  color:'white',
+  justifyContent:'center',
+  alignItems:'center',
+  backgroundColor:'green'
+
+})
+
+
+export default App;
---
+++ home/index.android.js
@@ -0,0 +1,13 @@
+
+import {View,Text} from 'react-native'
+
+function Home (){
+    return(
+
+        <View style={{flex:1,justifyContent:'center',alignItem:'center'}}>
+            <Text>Android</Text>
+        </View>
+    )
+}
+
+export default Home;
